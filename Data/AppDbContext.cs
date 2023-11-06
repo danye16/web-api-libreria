5dc98dca2dd61a8553caba4a0c15837f57cb8245
PC <danyel.16@gmail.com>
2023-11-05 23:26:47 -0600

    aaa

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,12 +3,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace libreria_JDPC.Data
 {
-    public class AppDbContext
+    public class AppDbContext:DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) 
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
 
-        } 
+        }
         public DbSet<Book> Books { get; set; }
     }
 }
--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -1,6 +1,24 @@
-﻿namespace libreria_JDPC.Data.Models
+﻿using System;
+
+namespace libreria_JDPC.Data.Models
 {
     public class Book
     {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public string Genero { get; set; }
+        public string Autor { get; set; }
+        public string CoverUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+
+
+
+
+
+
+
     }
 }
